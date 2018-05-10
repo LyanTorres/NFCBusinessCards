76f421e1ef1b74f6b107a5d4559f4c12397eaf8d
Lyan Torres Labiosa <ltorreslabiosa@fullsail.edu>
2018-05-10 18:45:13 -0400

    Pushing everything up until the presentation

--- TorresLyan_PP6/app/src/main/java/com/example/lyantorres/torreslyan_pp6/HomeScreenActivity.java
+++ TorresLyan_PP6/app/src/main/java/com/example/lyantorres/torreslyan_pp6/HomeScreenActivity.java
@@ -16,12 +16,8 @@ import android.widget.Toast;
 import com.example.lyantorres.torreslyan_pp6.fragments.ExpandedListFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.GenericTypeIndicator;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
